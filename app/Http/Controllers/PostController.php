5ee008aedce2ceba5c6c30545af1c9b6603bd0e5
Matvey <kesha5232@yande.ru>
2022-11-01 09:01:59 +0500

    v1

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+    public function store(Request $request)
+    {
+        return Post::create($request->all());
+    }
+    public function show(Post $post)
+    {
+        return $post;
+    }
+    public function update(Post $post, Request $request)
+    {
+        $post->update($request->all());
+        return $post;
+    }
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return [
+            "deleted" => true
+        ];
+    }
+}
+
---
+++ database/migrations/2022_10_31_061612_create_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->string("title_request");
+            $table->string("text_request");
+            $table->unsignedBigInteger('subdivision_id');
+            $table->unsignedBigInteger("category_id");
+            $table->date("date_request");
+            $table->timestamps();
+
+            $table->foreign("user_id")->references("id")->on('users');
+//            $table->foreign('subdivision_id')->references('id')->on('subdivisions');
+            $table->foreign("category_id")->references("id")->on("categories");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+};
